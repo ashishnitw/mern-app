d35e18e7bd3ae2f77207a4eb5913a57a665798e9
Ashish R <ashishr@Ashishs-MacBook-Air.local>
2025-07-04 19:29:33 +0530

    static file path

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
-import path from 'path';
 
 const port = process.env.PORT || 8000;
 
@@ -16,6 +16,7 @@ app.use('/api/products', productRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   // set static folder
+  const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   // any route that is not api will be redirected to index.html
